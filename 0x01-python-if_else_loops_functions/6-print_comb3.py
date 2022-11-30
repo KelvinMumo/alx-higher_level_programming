9bf51030727e013296fb00030920a6e6d6034b23
KelvinMumo <kelvinmumo977@gmail.com>
2022-11-30 11:37:06 +0000

    loops

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    dig1 = i / 10
+    dig2 = i % 10
+    if i == 89:
+        print("{:d}".format(i))
+    elif dig1 < dig2:
+        print("{:02d}".format(i), end= ", ")
