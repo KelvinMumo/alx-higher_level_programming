2d93b380107c9499072af170c56343eded6875ff
KelvinMumo <kelvinmumo977@gmail.com>
2023-01-03 11:42:14 +0000

    size of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Defines a class Square 
+    with a private instance attribute
+
+    """
+    def __init__(self, size):
+        """Initializes the square
+
+        Args:
+            size: size of the square
+
+        """
+        self.size = size
