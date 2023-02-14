765b19566887ef460df5c3799bc2ae8ceb53e97f
KelvinMumo <kelvinmumo977@gmail.com>
2023-02-14 19:00:16 +0000

    sql introduction

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- wget https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
+
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
